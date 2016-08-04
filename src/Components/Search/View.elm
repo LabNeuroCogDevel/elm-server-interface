2a8d036f1e0344afa33db894426ae19e813228ef
Will Foran <willforan@gmail.com>
2016-08-04 14:32:44 -0400

    center search bar and add tooltip

--- src/Components/Search/View.elm
+++ src/Components/Search/View.elm
@@ -10,24 +10,31 @@ import Utils.Html exposing (..)
 
 viewSearch : String -> (String -> msg) -> msg -> String -> (String -> msg) -> msg -> Html msg
 viewSearch txt onin onch ord ordin ordch =
-  span []
-    [ input
-        [ type' "text"
-        , onInput onin
-        , onChange (always onch)
-        , placeholder "Press Enter to Search"
-        , size 40
-        , value txt
-        ] []
-    , input
-        [ type' "text"
-        , onInput ordin
-        , onChange (always ordch)
-        , placeholder "Order by"
-        , size 20
-        , value ord
-        ] []
-    ]
+  div [class "search_bar"] 
+    [ span [class "tooltip_container"]
+      [ input
+          [ type' "text"
+          , onInput onin
+          , onChange (always onch)
+          , placeholder "Press Enter to Search"
+          , size 40
+          , value txt
+          ] []
+        , span [class "tooltip_text"] [text "e.g: age >=15; age < 30"]
+       ]
+   , span [class "tooltip_container"]
+       [ input
+            [ type' "text"
+            , onInput ordin
+            , onChange (always ordch)
+            , placeholder "Order by"
+            , size 20
+            , value ord
+            ] []
+       , span [class "tooltip_text"] [text "e.g: age"]
+       ]
+    
+  ]
 
 view : SearchModel k -> Html (SearchMsg k)
 view model = viewSearch (searchString model) SearchStringChanged SearchEnter (orderString model) OrdStringChanged OrdEnter
